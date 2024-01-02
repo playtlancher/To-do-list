bf53a5a2ed848cd804af067121c3adf476d2dd43
Yoroslav Venediktov <playtlancher@gmail.com>
2024-01-02 13:16:26 +0200

    add checkbox status

--- task.js
+++ task.js
@@ -6,24 +6,30 @@ let rows = 5;
 
 export function saveTasks() {
     let taskList = document.getElementById("task-list");
+    // let checkbox = document.getElementsByClassName("checkbox");
     let tasks = [];
+    let taskCheckStatus = [];
     if (taskList) {
         for (let i = 0; i < taskList.children.length; i++) {
             let taskSpan = taskList.children[i].querySelector("span");
+            let status = taskList.children[i].querySelector("input").checked;
+            taskCheckStatus.push(status);
             if (taskSpan) {
                 tasks.push(taskSpan.textContent);
             }
         }
         localStorage.setItem("tasks", JSON.stringify(tasks));
+        localStorage.setItem("status", JSON.stringify(taskCheckStatus));
     }
 }
 
 export function loadTasks() {
     let tasks = JSON.parse(localStorage.getItem("tasks"));
+    let status = JSON.parse(localStorage.getItem("status"));
 
-    if (tasks) {
+    if (tasks && status) {
         for (let i = 0; i < tasks.length; i++) {
-            addTask(tasks[i]);
+            addTask(tasks[i],status[i]);
         }
     }
     setTheme(localStorage.getItem("theme"));
@@ -41,7 +47,7 @@ export function addTaskByButton() {
 
 }
 
-export function addTask(taskText) {
+export function addTask(taskText , status = false) {
     let taskList = document.getElementById("task-list");
 
     let taskItem = document.createElement("li");
@@ -69,6 +75,11 @@ export function addTask(taskText) {
     })
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.checked = status;
+    if (checkbox.checked) {
+        taskSpan.style.transform = "rotateX(360deg)";
+        taskSpan.style.textDecoration = "line-through";
+    }
     checkbox.addEventListener("change", function () {
         if (checkbox.checked) {
             taskSpan.style.transform = "rotateX(360deg)";
@@ -77,6 +88,7 @@ export function addTask(taskText) {
             taskSpan.style.transform = "rotateX(0deg)";
             taskSpan.style.textDecoration = "none";
         }
+        saveTasks()
     })
     taskItem.classList.add('fade-in');
 
